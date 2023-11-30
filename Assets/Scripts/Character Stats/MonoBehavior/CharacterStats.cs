d6678261146578f92c1ff7a88a337a0616f21df3
LouistheOutlaw <931429541@qq.com>
2023-11-30 22:48:22 +0800

    文件夹补全
    不包括 /Library

---
+++ Assets/Scripts/Character Stats/MonoBehavior/CharacterStats.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class CharacterStats : MonoBehaviour
+{
+    public CharacterData_SO characterData;
+    public AttackData_SO attackData;
+
+    [HideInInspector]
+    public bool isCritical;
+    
+    #region Read form Data_SO
+    public int MaxHealth 
+    {
+        get{ if (characterData != null) return characterData.maxHealth; else return 0; }
+        set{ characterData.maxHealth = value;}    
+    }
+
+    public int CurrentHealth
+    {
+        get { if (characterData != null) return characterData.currentHealth; else return 0; }
+        set { characterData.currentHealth = value; }
+    }
+
+    public int BaseDefence
+    {
+        get { if (characterData != null) return characterData.baseDefence; else return 0; }
+        set { characterData.baseDefence = value; }
+    }
+
+    public int CurrentDefence
+    {
+        get { if (characterData != null) return characterData.currentDefence; else return 0; }
+        set { characterData.currentDefence = value; }
+    }
+    #endregion
+
+    #region Character Combat
+
+    public void TakeDamage(CharacterStats attacker,CharacterStats defender)
+    {
+        int damage = Mathf.Max(attacker.CurrentDamage() - defender.CurrentDefence,0);
+        CurrentHealth = Mathf.Max(CurrentHealth - damage, 0);
+
+        if(attacker.isCritical)
+        {
+            defender.GetComponent<Animator>().SetTrigger("Hit");
+        }
+        //TODO:Update UI
+        //TODO:¾­Ñéupdate
+    }
+
+    private int CurrentDamage()
+    {
+        float coreDamage = UnityEngine.Random.Range(attackData.minDamge, attackData.maxDamage);
+
+        if (isCritical)
+        {
+            coreDamage *= attackData.criticalMultiplier;
+            Debug.Log("±©»÷£¡" + coreDamage);
+        }
+
+        return (int)coreDamage;
+    }
+
+    #endregion
+}
